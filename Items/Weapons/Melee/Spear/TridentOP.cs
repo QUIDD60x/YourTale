2fa66aaa353ea5c7824f6a8ab6e58979ecdef47b
BuildTools <unconfigured@null.spigotmc.org>
2022-10-18 17:39:34 -0700

    Adding a spear for testing (not finished)

---
+++ Items/Weapons/Melee/Spear/TridentOP.cs
@@ -0,0 +1,77 @@
+﻿using yourtale.Projectiles.Spears;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace yourtale.Items.Weapons.Melee.Spear
+{
+	public class TridentOP : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Tident of Posideon");
+			Tooltip.SetDefault("Smells fishy.");
+
+			ItemID.Sets.SkipsInitialUseSound[Item.type] = true; // This skips use animation-tied sound playback, so that we're able to make it be tied to use time instead in the UseItem() hook.
+			ItemID.Sets.Spears[Item.type] = true; // This allows the game to recognize our new item as a spear.
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			// Common Properties
+			Item.rare = ItemRarityID.Pink; // Assign this item a rarity level of Pink
+			Item.value = Item.sellPrice(gold: 3, silver: 15); // The number and type of coins item can be sold for to an NPC
+
+			// Use Properties
+			Item.useStyle = ItemUseStyleID.Shoot; // How you use the item (swinging, holding out, etc.)
+			Item.useAnimation = 12; // The length of the item's use animation in ticks (60 ticks == 1 second.)
+			Item.useTime = 18; // The length of the item's use time in ticks (60 ticks == 1 second.)
+			Item.UseSound = SoundID.Item71; // The sound that this item plays when used.
+			Item.autoReuse = true; // Allows the player to hold click to automatically use the item again. Most spears don't autoReuse, but it's possible when used in conjunction with CanUseItem()
+
+			// Weapon Properties
+			Item.damage = 25;
+			Item.knockBack = 6.5f;
+			Item.noUseGraphic = true; // When true, the item's sprite will not be visible while the item is in use. This is true because the spear projectile is what's shown so we do not want to show the spear sprite as well.
+			Item.DamageType = DamageClass.Melee;
+			Item.noMelee = true; // Allows the item's animation to do damage. This is important because the spear is actually a projectile instead of an item. This prevents the melee hitbox of this item.
+
+			// Projectile Properties
+			Item.shootSpeed = 3.7f; // The speed of the projectile measured in pixels per frame.
+			Item.shoot = ModContent.ProjectileType<TridentOProj>(); // The projectile that is fired from this weapon
+		}
+
+		public override bool CanUseItem(Player player)
+		{
+			// Ensures no more than one spear can be thrown out, use this when using autoReuse
+			return player.ownedProjectileCounts[Item.shoot] < 1;
+		}
+
+		public override bool? UseItem(Player player)
+		{
+			// Because we're skipping sound playback on use animation start, we have to play it ourselves whenever the item is actually used.
+			if (!Main.dedServ && Item.UseSound.HasValue)
+			{
+				SoundEngine.PlaySound(Item.UseSound.Value, player.Center);
+			}
+
+			return null;
+		}
+
+		// Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+				.AddIngredient(ItemID.Trident)
+				.AddIngredient(ItemID.HallowedBar, 8)
+				.AddIngredient(ItemID.Coral, 5)
+				.AddIngredient(ItemID.SharkFin, 3)
+				.AddIngredient(ItemID.GoldCrown)
+				.AddTile(TileID.MythrilAnvil)
+				.Register();
+		}
+	}
+}
---
+++ Projectiles/Spears/TridentOProj.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace yourtale.Projectiles.Spears
+{
+	public class TridentOProj : ModProjectile
+	{
+		// Define the range of the Spear Projectile. These are overrideable properties, in case you'll want to make a class inheriting from this one.
+		protected virtual float HoldoutRangeMin => 24f;
+		protected virtual float HoldoutRangeMax => 96f;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Posideon's Trident");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.CloneDefaults(ProjectileID.Spear); // Clone the default values for a vanilla spear. Spear specific values set for width, height, aiStyle, friendly, penetrate, tileCollide, scale, hide, ownerHitCheck, and melee.
+		}
+
+		public override bool PreAI()
+		{
+			Player player = Main.player[Projectile.owner]; // Since we access the owner player instance so much, it's useful to create a helper local variable for this
+			int duration = player.itemAnimationMax; // Define the duration the projectile will exist in frames
+
+			player.heldProj = Projectile.whoAmI; // Update the player's held projectile id
+
+			// Reset projectile time left if necessary
+			if (Projectile.timeLeft > duration)
+			{
+				Projectile.timeLeft = duration;
+			}
+
+			Projectile.velocity = Vector2.Normalize(Projectile.velocity); // Velocity isn't used in this spear implementation, but we use the field to store the spear's attack direction.
+
+			float halfDuration = duration * 0.5f;
+			float progress;
+
+			// Here 'progress' is set to a value that goes from 0.0 to 1.0 and back during the item use animation.
+			if (Projectile.timeLeft < halfDuration)
+			{
+				progress = Projectile.timeLeft / halfDuration;
+			}
+			else
+			{
+				progress = (duration - Projectile.timeLeft) / halfDuration;
+			}
+
+			// Move the projectile from the HoldoutRangeMin to the HoldoutRangeMax and back, using SmoothStep for easing the movement
+			Projectile.Center = player.MountedCenter + Vector2.SmoothStep(Projectile.velocity * HoldoutRangeMin, Projectile.velocity * HoldoutRangeMax, progress);
+
+			// Apply proper rotation to the sprite.
+			if (Projectile.spriteDirection == -1)
+			{
+				// If sprite is facing left, rotate 45 degrees
+				Projectile.rotation += MathHelper.ToRadians(45f);
+			}
+			else
+			{
+				// If sprite is facing right, rotate 135 degrees
+				Projectile.rotation += MathHelper.ToRadians(135f);
+			}
+
+			// Avoid spawning dusts on dedicated servers
+			if (!Main.dedServ)
+			{
+				// These dusts are added later, for the 'ExampleMod' effect
+				if (Main.rand.NextBool(3))
+				{
+					Dust dust;
+					// You need to set position depending on what you are doing. You may need to subtract width/2 and height/2 as well to center the spawn rectangle.
+					Vector2 position = Main.LocalPlayer.Center;
+					dust = Main.dust[Terraria.Dust.NewDust(position, 30, 30, 14, 0f, 0f, 0, new Color(255, 255, 255), 1f)];
+					dust.noGravity = true;
+					dust.fadeIn = 0.627907f;
+
+				}
+
+			}
+
+				return false; // Don't execute vanilla AI.
+		}
+	}
+}
