19d0257b68daea6848786982cbcedff365b2de15
BuildTools <unconfigured@null.spigotmc.org>
2022-11-26 23:09:04 -0800

    Added a new main menu theme <3

---
+++ ModMenu.cs
@@ -0,0 +1,43 @@
+﻿using Terraria;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria.ModLoader;
+using Terraria.Audio;
+using Terraria.ID;
+
+namespace yourtale
+{
+	public class ExampleModMenu : ModMenu
+	{
+		// I'm not going to be using any special textures simply because I can't draw.
+		// private const string menuAssetPath = "yourtale/Assets/Textures/Menu"; // This Creates a constant variable representing the texture path, so we don't have to write it out multiple times
+
+		public override Asset<Texture2D> Logo => ModContent.Request<Texture2D>($"yourtale/icon");
+
+		// public override Asset<Texture2D> SunTexture => ModContent.Request<Texture2D>($"{menuAssetPath}/ExampleSun");
+
+		// public override Asset<Texture2D> MoonTexture => ModContent.Request<Texture2D>($"{menuAssetPath}/ExampliumMoon");
+
+		public override int Music => MusicLoader.GetMusicSlot(Mod, "Assets/Music/YourTaleTheme");
+
+		// public override ModSurfaceBackgroundStyle MenuBackgroundStyle => ModContent.GetInstance<ExampleSurfaceBackgroundStyle>();
+
+		public override string DisplayName => "Your Tale menu";
+
+		public new bool IsSelected => MenuLoader.CurrentMenu == this;
+
+		public override void OnSelected()
+		{
+			SoundEngine.PlaySound(SoundID.Item9); // Plays a sound when this ModMenu is selected
+		}
+
+		public override bool PreDrawLogo(SpriteBatch spriteBatch, ref Vector2 logoDrawCenter, ref float logoRotation, ref float logoScale, ref Color drawColor)
+		{
+			drawColor = Main.DiscoColor; // Changes the draw color of the logo
+			logoScale *= 2.2f;
+			return true;
+			
+		}
+	}
+}
