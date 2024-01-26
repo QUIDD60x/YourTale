229b8d562cc13e26ea6a64f6e219fe2b635f3a62
Quidd <73556700+QUIDD60x@users.noreply.github.com>
2024-01-26 12:29:00 -0800

    Update AmbrosiaLegs.cs

--- Items/Armor/Ambrosia/AmbrosiaLegs.cs
+++ Items/Armor/Ambrosia/AmbrosiaLegs.cs
@@ -9,13 +9,13 @@ namespace yourtale.Items.Armor.Ambrosia
     {
         public override void SetDefaults()
         {
+            // Item properties
             Item.width = 25;
             Item.height = 700;
-
+            Item.defense = 8;
+            // Item value
             Item.value = 20000;
             Item.rare = ItemRarityID.LightRed;
-
-            Item.defense = 8;
         }
 
         public override void UpdateAccessory(Player player, bool hideVisual)
